65fffc051d23fe83156c956ebb499081b5d2fc77
Streif44 <diego.atalayaramirez@alum.uca.es>
2025-11-23 22:49:46 +0100

    Cambios a lineaDeCompra.java

--- src/jungle/lineaDeCompra.java
+++ src/jungle/lineaDeCompra.java
@@ -1,16 +1,35 @@
 package jungle;
+import java.time.format.DateTimeFormatter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 public class LineaDeCompra
 {
-private int cod_lc;
-private static int num_cod_lc = 0;
-private int cantidad_lc;
-private float precio_lc;
-private Medicamento m;
-public LineaDeCompra(int cantidad_, Medicamento m_)
-{
- this.cod_lc = num_cod_lc++;
+ private int cod_lc;
+ private int cantidad_lc;
+ private float precio_lc;
+ private Medicamento m;
+ public LineaDeCompra(int cantidad_, Medicamento m_)
+ {
  this.cantidad_lc = cantidad_;
-this.m = m_;
+ this.m = m_;
  this.precio_lc = this.m.getPrecioMedicamento() * this.cantidad_lc;
-}
+ }
+ public int getCod_lc()
+ {
+ return this.cod_lc;
+ }
+ public int getCantidad_lc()
+ {
+ return this.cantidad_lc;
+ }
+ public float getPrecio_lc()
+ {
+ return this.precio_lc;
+ }
+ public Medicamento getMedicamento()
+ {
+ return this.m;
+ }
 }
