27c3eb577ec3625a67aef815d50ab607e87ed0ea
Streif44 <diego.atalayaramirez@alum.uca.es>
2025-11-23 20:53:45 +0100

    Creando lineaDeCompra.java

---
+++ src/jungle/lineaDeCompra.java
@@ -0,0 +1,16 @@
+package jungle;
+public class LineaDeCompra
+{
+private int cod_lc;
+private static int num_cod_lc = 0;
+private int cantidad_lc;
+private float precio_lc;
+private Medicamento m;
+public LineaDeCompra(int cantidad_, Medicamento m_)
+{
+ this.cod_lc = num_cod_lc++;
+ this.cantidad_lc = cantidad_;
+this.m = m_;
+ this.precio_lc = this.m.getPrecioMedicamento() * this.cantidad_lc;
+}
+}
